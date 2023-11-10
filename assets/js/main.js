5b79f742e9ca66b218149b911099aaae0c7451b5
Alfaraby Indra <alfarabyindra@gmail.com>
2023-11-10 21:59:31 +0700

    repuload

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,42 @@ offcanvas.addEventListener("show.bs.offcanvas", function () {
 offcanvas.addEventListener("hidden.bs.offcanvas", function () {
   stickyTop.style.overflow = "hidden";
 });
+
+//fungsi utk membuat enable/disable scroll
+const rootElement = document.querySelector(":root");
+function disableScroll() {
+  scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+
+  window.onscroll = function () {
+    window.scrollTo(screenTop, scrollLeft);
+  };
+
+  rootElement.style.scrollBehavior = "auto";
+}
+
+function enableScroll() {
+  window.onscroll = function () {};
+  rootElement.style.scrollBehavior = "smooth";
+  localStorage.setItem("opened", "true");
+}
+
+if (!localStorage.getItem("opened")) {
+  disableScroll();
+}
+
+//script form google sheet ketika sudah mengisi kehadiran pesta pernikahan
+window.addEventListener("load", function () {
+  const form = document.getElementById("my-form");
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const data = new FormData(form);
+    const action = e.target.action;
+    fetch(action, {
+      method: "POST",
+      body: data,
+    }).then(() => {
+      alert("Konfirmasi Kehadiran Berhasil Terkirim!");
+    });
+  });
+});
